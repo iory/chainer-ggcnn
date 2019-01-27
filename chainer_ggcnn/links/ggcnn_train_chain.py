190fdd9380010cd8fec975a9ddb838e90ba64807
iory <ab.ioryz@gmail.com>
2019-01-27 16:48:35 +0900

    [links] Add GGCNN predict

--- chainer_ggcnn/links/ggcnn_train_chain.py
+++ chainer_ggcnn/links/ggcnn_train_chain.py
@@ -9,6 +9,10 @@ class GGCNNTrainChain(chainer.Chain):
         with self.init_scope():
             self.predictor = predictor
 
+    def predict(self, imgs):
+        pred_pos, pred_sin, pred_cos, pred_width = self.predictor(imgs)
+        return pred_pos, pred_sin, pred_cos, pred_width
+
     def forward(self,
                 imgs,
                 positions,
