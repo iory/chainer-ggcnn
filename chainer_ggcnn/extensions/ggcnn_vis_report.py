a2e1f2cc34ba0331442678b5a7682c4881f84eef
iory <ab.ioryz@gmail.com>
2019-01-27 17:46:23 +0900

    [extensions] Fixed

--- chainer_ggcnn/extensions/ggcnn_vis_report.py
+++ chainer_ggcnn/extensions/ggcnn_vis_report.py
@@ -42,6 +42,7 @@ class GGCNNVisReport(chainer.training.extensions.Evaluator):
                 six.moves.zip(
                     depths, pred_poses, pred_sines, pred_coses, pred_widthes)):
             depth = depth.squeeze()
+            pred_pos = pred_pos.squeeze()
             pred_sin = pred_sin.squeeze()
             pred_cos = pred_cos.squeeze()
             pred_width = pred_width.squeeze()
