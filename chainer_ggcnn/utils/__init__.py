5c8d3346960888dcbd65c5d01cc920c033dcf2a1
iory <ab.ioryz@gmail.com>
2019-01-27 18:40:46 +0900

    [utils] Add grasp

--- chainer_ggcnn/utils/__init__.py
+++ chainer_ggcnn/utils/__init__.py
@@ -1,2 +1,3 @@
+from chainer_ggcnn.utils.grasp import BoundingBoxes  # NOQA
 from chainer_ggcnn.utils.makedirs import makedirs  # NOQA
 from chainer_ggcnn.utils.prepare_output_dir import prepare_output_dir  # NOQA
