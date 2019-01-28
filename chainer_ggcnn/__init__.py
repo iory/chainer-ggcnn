4dfa8494eadd122373bd9e570cd7205aed24bf59
iory <ab.ioryz@gmail.com>
2019-01-28 10:06:55 +0900

    [datasets] Add cached_gdown

--- chainer_ggcnn/__init__.py
+++ chainer_ggcnn/__init__.py
@@ -5,6 +5,7 @@ __version__ = pkg_resources.get_distribution(
     'chainer_ggcnn').version
 
 
+import chainer_ggcnn.datasets  # NOQA
 import chainer_ggcnn.extensions  # NOQA
 import chainer_ggcnn.links  # NOQA
 import chainer_ggcnn.utils  # NOQA
---
+++ chainer_ggcnn/datasets/__init__.py
@@ -0,0 +1,1 @@
+from chainer_ggcnn.datasets.download import cached_gdown_download  # NOQA
